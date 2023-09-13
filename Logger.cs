2de87a249c30b714855e179a454ac9e25faf4076
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-13 20:20:30 +0300

    Created Log constructor
    It defines current project directory, sets file name and defines path to this logFile

--- Logger.cs
+++ Logger.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.IO;
+
 namespace Logger
 {
     public class Logger
     {
-        
+        private string currentDirectory;
+        private string logFileName;
+        private string logFilePath;
+
+        public Logger(string logFileName)
+        {
+            currentDirectory = Directory.GetCurrentDirectory();
+            this.logFileName = logFileName + ".txt";
+            logFileName = currentDirectory + "/" + this.logFileName;
+        }
     }
 }
 
