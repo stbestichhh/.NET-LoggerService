8b366559dafbd3e73f59cb477d15313617503b66
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-13 20:42:40 +0300

    Redefined Log method to edit log file

--- Logger.cs
+++ Logger.cs
@@ -3,17 +3,32 @@ using System.IO;
 
 namespace Logger
 {
-    public class Logger
+    public class Logger : LogBase
     {
         private string currentDirectory;
         private string logFileName;
-        private string logFilePath;
+        private string logFilePath;        
 
         public Logger(string logFileName)
         {
             currentDirectory = Directory.GetCurrentDirectory();
             this.logFileName = logFileName + ".txt";
-            logFileName = currentDirectory + "/" + this.logFileName;
+            logFilePath = currentDirectory + "/" + this.logFileName;
+        }
+
+        private string DefineLogDateTime()
+        {
+            return $"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}";
+        }        
+
+        public override void Log(string message)
+        {
+            using (StreamWriter writer = File.AppendText(logFilePath))
+            {
+                writer.Write($"\r\n{currentDirectory} | Log into : {logFileName}| {DefineLogDateTime()}");
+                writer.WriteLine($"\t: {message}");
+                writer.WriteLine();
+            }
         }
     }
 }
