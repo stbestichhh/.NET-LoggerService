ee29f68ceaf9dc8055bd3c021a0b0a691fe9b8aa
Kiril Yakymchuk <stbestich@gmail.com>
2023-09-21 16:29:15 +0300

    Writing to file is async now

--- Classes/LoggerExtensions.cs
+++ Classes/LoggerExtensions.cs
@@ -1,15 +1,16 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Logger
 {
     static class LoggerClassExtensions
     {
-        public static void CreateLogMessage(this StreamWriter writer, string nameSpace, string message, LogLevels logLevel, Exception exception = null, int logId = 0)
+        public static async Task CreateLogMessage(this StreamWriter writer, string nameSpace, string message, LogLevels logLevel, Exception exception = null, int logId = 0)
         {
             string logInfo = $"{DefineLogDateTime()}";
 
-            writer.Write($"\r\n{logInfo}|{logId}|{nameSpace}|{logLevel}|{message} : {exception}\n");
+            await writer.Write($"\r\n{logInfo}|{logId}|{nameSpace}|{logLevel}|{message} : {exception}\n");
         }
 
         private static string DefineLogDateTime()
