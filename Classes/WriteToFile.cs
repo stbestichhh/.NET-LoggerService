e4782f4111bab94c06893e251a8a35c7b1e1ada8
Kiril Yakymchuk <stbestich@gmail.com>
2023-09-22 08:52:49 +0300

    Update WriteToFile.cs

--- Classes/WriteToFile.cs
+++ Classes/WriteToFile.cs
@@ -8,9 +8,9 @@ namespace Logger
     {
         public async Task Write(string logFilePath, string nameSpace, string message, LogLevels logLevel, Exception exception = null, int logId = 0)
         {
-            await using (StreamWriter writer = File.AppendText(logFilePath))
+            using (StreamWriter writer = File.AppendText(logFilePath))
             {
-                writer.CreateLogMessage(nameSpace, message, logLevel, exception, logId);
+                await writer.CreateLogMessage(nameSpace, message, logLevel, exception, logId);
             }
         }
     }
